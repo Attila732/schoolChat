847e24a098f6bc21d953a228028051cfef98708f
attila <attila@Attila.catv.pool.telekom.hu>
2023-10-16 09:12:37 +0200

    Félkész

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavComponent } from './nav/nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Environments } from './enviroments';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignupComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    AngularFireModule.initializeApp(Environments.firebaseConfig),
+    AngularFireDatabaseModule 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/enviroments.ts
@@ -0,0 +1,23 @@
+export const Environments = {
+    production:false,
+    firebaseConfig : {
+
+        apiKey: "AIzaSyC_a18ig7hjnw84QUhEsLuHh2Fm_1N3h1M",
+      
+        authDomain: "napraforgo-318ac.firebaseapp.com",
+
+        databaseURL: "https://napraforgo-318ac-default-rtdb.europe-west1.firebasedatabase.app",
+      
+        projectId: "napraforgo-318ac",
+      
+        storageBucket: "napraforgo-318ac.appspot.com",
+      
+        messagingSenderId: "960903367422",
+      
+        appId: "1:960903367422:web:312fd92e8f841b468b73af",
+      
+        measurementId: "G-JSW94EBQT9"
+      
+      }
+      
+}
